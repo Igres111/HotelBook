f262551b2355b71ae9fb860274f200fc37ded740
Igres111 <sergikaralashvili@gmail.com>
2025-03-14 23:12:50 +0400

    Add project files.

---
+++ DataAccess/Entities/Hotel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string HotelImage { get; set; } = string.Empty;
+        public int Rating { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? Delete { get; set; }
+    }
+}
---
+++ HotelBook/Program.cs
@@ -0,0 +1,40 @@
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connection = Environment.GetEnvironmentVariable("connection");
+var key = Environment.GetEnvironmentVariable("Key");
+if (string.IsNullOrEmpty(key))
+{
+    throw new Exception("JWT secret key is not set in the environment variables.");
+}
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(connection);
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
